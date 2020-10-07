2c256da6640e9c08c210b1454fc106ef64e3ba7c
Ashish <drillmaps@gmail.com>
2020-10-07 16:45:16 -0700

    Initial Commit

---
+++ CardGameUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGameUI
+//
+//  Created by Ashish Ashish on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
