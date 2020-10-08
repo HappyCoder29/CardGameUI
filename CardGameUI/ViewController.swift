c5cddac112fe13e0722d13f9a6035412e52d64e7
Ashish <ashish@drillmaps.com>
2020-10-07 17:07:34 -0700

    Checking in code for CardGameUI

--- CardGameUI/ViewController.swift
+++ CardGameUI/ViewController.swift
@@ -9,10 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var player1Img1: UIImageView!
+    
+    @IBOutlet weak var player1Img2: UIImageView!
+    
+    @IBOutlet weak var player1Img3: UIImageView!
+    
+    @IBOutlet weak var player2Img1: UIImageView!
+    
+    @IBOutlet weak var player2Img2: UIImageView!
+    
+    @IBOutlet weak var player3Img3: UIImageView!
+    
+    
+    @IBOutlet weak var lblWinner: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func playGame(_ sender: Any) {
+    }
+    
+    
+    
+    
 
 
 }
